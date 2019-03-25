a04ddc0284ca798ceaacc215398d8b16eeaa7427
garikmk <garikmk@gmail.com>
2019-03-25 11:20:26 +0300

    retarget to netstandard bind to qpublishing.aspnetcore 3.3.1

--- EntityFrameworkCore.CodeGeneration/PublishNugetPackage.cmd
+++ EntityFrameworkCore.CodeGeneration/PublishNugetPackage.cmd
@@ -1,2 +1,2 @@
 dotnet pack /p:NuspecFile=NugetPackage.nuspec
-dotnet nuget push bin\Debug\QP8.EntityFrameworkCore.1.0.0.nupkg -s file://mscbuild02.artq.com/Packages/
+dotnet nuget push bin\Debug\QP8.EntityFrameworkCore.1.0.1.nupkg -s file://mscbuild02.artq.com/Packages/
