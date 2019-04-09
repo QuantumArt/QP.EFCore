8157a0a4b131a8be8daca70540da5f96433fb68e
garikmk <garikmk@gmail.com>
2019-04-09 12:59:52 +0300

    fix backward reference

--- EntityFrameworkCore.CodeGeneration/PublishNugetPackage.cmd
+++ EntityFrameworkCore.CodeGeneration/PublishNugetPackage.cmd
@@ -1,2 +1,2 @@
 dotnet pack /p:NuspecFile=NugetPackage.nuspec
-dotnet nuget push bin\Debug\QP8.EntityFrameworkCore.1.0.2.nupkg -s file://mscbuild02.artq.com/Packages/
+dotnet nuget push bin\Debug\QP8.EntityFrameworkCore.1.0.3.nupkg -s file://mscbuild02.artq.com/Packages/
