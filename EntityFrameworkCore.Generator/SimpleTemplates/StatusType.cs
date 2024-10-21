8b5cc9584b614dec32f383e75f280132645ffda5
CelutP <CelutP@gmail.com>
2024-10-21 16:41:19 +0300

    Introduce SiteSpecificInfo class

--- EntityFrameworkCore.Generator/SimpleTemplates/StatusType.cs
+++ EntityFrameworkCore.Generator/SimpleTemplates/StatusType.cs
@@ -8,16 +8,15 @@ namespace Quantumart.QP8.EntityFrameworkCore.Generator.SimpleTemplates
         {
             cancellationToken.ThrowIfCancellationRequested();
             return @$"{context.Settings.GeneratedCodePrefix}
-using System;
 
 namespace {ns}
 {{
     public partial class StatusType
     {{
-        public Int32 Id {{ get; set; }}
+        public int Id {{ get; set; }}
         public string StatusTypeName {{ get; set; }}
         public int Weight {{ get; set; }}
-        public Int32 SiteId {{ get; set; }}
+        public int SiteId {{ get; set; }}
     }}
 }}";
         }
